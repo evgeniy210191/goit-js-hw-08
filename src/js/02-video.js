64ca2a7b59d9f5a3c2625b58b1df92248b2c9c41
evgeniy210191 <bees127@ukr.net>
2023-03-11 01:50:56 +0200

    f

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,7 @@
 import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+console.log(player);
 
 player.on('play', function () {
   console.log('played the video!');
