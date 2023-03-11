98cc1444c3ceeed1423b0576cc4f83f0d7c32f83
evgeniy210191 <bees127@ukr.net>
2023-03-11 03:19:32 +0200

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,44 @@
 import Player from '@vimeo/player';
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-console.log(player);
 
-player.on('play', function () {
-  console.log('played the video!');
+const player = new Player('vimeo-player', {
+  id: 19231868,
+  width: 640,
 });
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
-const onPlay = function (data) {
-  // data is an object containing properties specific to that event
+// player.on('play', function () {
+//     duration: 61.857;
+//     percent: 0.049;
+//   seconds: 3.034;
+//   console.log('hello');
+//   
+// });
+const onPlay = function () {
+  console.log('hello');
+  
 };
 
 player.on('play', onPlay);
+
 player.off('play', onPlay);
+
+const time = localStorage.getItem('time')
+
+player
+  .setCurrentTime(time)
+  .then(function (seconds) {
+    console.log(seconds);
+    
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.log('seconds');// the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        console.log('seconds');// some other error occurred
+        break;
+    }
+  });
+
+  // _.throttle(updatePosition, 100);
--- src/js/fn01/api.js
+++ src/js/fn01/api.js
@@ -1,6 +1,6 @@
 export default function displayListGallery(listGallery) {
   return listGallery
-    .map((item) => {
+    .map(item => {
       return `
         <a class="gallery__item" href="${item.original}">
           <img class="gallery__image" 
