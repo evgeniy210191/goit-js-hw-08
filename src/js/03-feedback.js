2609fef38c4e3857dfcc4ebb8644ff5fa523eb40
evgeniy210191 <bees127@ukr.net>
2023-03-13 17:25:38 +0200

    end

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,16 @@
 import throttle from './fn03/api.js';
-const feedback_form_state = {};
+const FeedbackFormState = {};
 const form = document.querySelector('.feedback-form');
 
 form.addEventListener(
   'input',
   throttle(function (event) {
-    feedback_form_state[event.target.name] = event.target.value;
+    FeedbackFormState[event.target.name] = event.target.value;
     localStorage.setItem(
       'feedback-form-state',
-      JSON.stringify(feedback_form_state)
+      JSON.stringify(FeedbackFormState)
     );
-    console.log(feedback_form_state);
+    
     
   }, 1000)
 );
@@ -18,12 +18,23 @@ addEventListener('load', function (event) {
   const data = localStorage.getItem('feedback-form-state');
   if (data) {
     const dataParse = JSON.parse(data);
-    form.elements.email.value = dataParse.email;
-    form.elements.message.value = dataParse.message;
+    if (dataParse.email) {
+      form.elements.email.value = dataParse.email;
+    }
+    if (dataParse.message) {
+      form.elements.message.value = dataParse.message;
+    } 
+    return
   }
 });
+
+
 form.addEventListener('submit', function (event) {
   event.preventDefault();
+  if (form.elements.email.value === '' || form.elements.message.value === '') {
+    return
+  }
+  console.log(FeedbackFormState);
   localStorage.removeItem('feedback-form-state');
   form.reset();
 });
